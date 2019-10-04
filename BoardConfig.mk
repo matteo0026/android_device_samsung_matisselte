83a1ef6e07108e2d8a6240aadcef5326b8217ae2
matteo0026 <marmotta200026@gmail.com>
2019-10-04 19:39:43 +0200

    Added README and added some good flags in BoardConfig

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -31,7 +31,13 @@ TW_INPUT_BLACKLIST := "hbtp_vm"
 
 # TWRP
 BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_23x41.h\"
-TW_THEME := portrait_hdpi
+TW_THEME := landscape_hdpi
 RECOVERY_SDCARD_ON_DATA := true
 RECOVERY_GRAPHICS_USE_LINELENGTH := true
-
+TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
+TW_DEFAULT_BRIGHTNESS := 162
+TW_MAX_BRIGHTNESS := 255
+TW_MTP_DEVICE := "/dev/mtp_usb"
+TW_NO_REBOOT_BOOTLOADER := true
+TW_HAS_DOWNLOAD_MODE := true
+TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/platform/msm_hsusb/gadget/lun%d/file"
