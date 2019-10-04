8974f192ce4eb5c2c6adb738299c0fe9cb0574c0
matteo0026 <marmotta200026@gmail.com>
2019-10-04 22:42:09 +0200

    Fix here too

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -14,7 +14,7 @@ TARGET_PREBUILT_KERNEL := device/samsung/matisselte/kernel
 BOARD_KERNEL_CMDLINE := androidboot.console=null androidboot.hardware=qcom androidboot.selinux=permissive androidboot.bootdevice=msm_sdcc.1
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000 --tags_offset 0x01e00000 --dt $(LOCAL_PATH)/dt.img
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000 --tags_offset 0x01e00000 --dt device/samsung/matisselte/dt.img
 BOARD_NEEDS_LZMA_MINIGZIP := true
 
 # Recovery
